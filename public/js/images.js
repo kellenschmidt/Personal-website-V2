6f83ac871fa6227d05e36b14a5f7194cf5cc2ec7
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-08-17 09:34:30 -0500

    Fixed background on all iOS devices not just iPhone

--- public/js/images.js
+++ public/js/images.js
@@ -7,21 +7,20 @@ var background = {crop: "scale", fetch_format: "auto", quality: "auto", width: 2
 // Apply cloudinary URL to background-image
 var imageUrl = cl.url("background-050", background);
 
-// Determine if browser is iPhone Safari
-    var ua = window.navigator.userAgent;
-    var iPhone = !!ua.match(/iPhone/i);
-    var webkit = !!ua.match(/WebKit/i);
-    var iPhoneSafari = iPhone && webkit && !ua.match(/CriOS/i);
+// Determine if browser is iOS Safari
+var ua = window.navigator.userAgent;
+var iOS = !!ua.match(/iPhone|iPad|iPod/i);
+var webkit = !!ua.match(/WebKit/i);
+var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
-    // If device/browser is iPhone Safari then set background-attachment to compatible value
-    if(iPhoneSafari) {
-        $(".background-fallback-image").css("background-image", "url(" + imageUrl + ")");
-        $(".background-fallback-image").css("display", "inherit");
-    } else {
-        $(".title-page-wallpaper").css("background-image", "url(" + imageUrl + ")");
-        $(".background-fallback-image").css("display", "none");
-    }
-
+// If device/browser is iOS Safari then set background-attachment to compatible value
+if(iOSSafari) {
+    $(".background-fallback-image").css("background-image", "url(" + imageUrl + ")");
+    $(".background-fallback-image").css("display", "inherit");
+} else {
+    $(".title-page-wallpaper").css("background-image", "url(" + imageUrl + ")");
+    $(".background-fallback-image").css("display", "none");
+}
 
 // Set configuration for chip transformation
 var chip = {crop: "scale", radius: "max", fetch_format: "auto", quality: "auto", width:"120"};
